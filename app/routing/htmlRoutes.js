45a0fbb31c763001330557729aaea4ade94bd602
A.Sofiane Guerfi <asofianeguerfi@calvisitor-10-105-161-89.calvisitor.1918.berkeley.edu>
2018-02-08 18:31:20 -0800

    heroku Up

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app){
 	});
 
 	// Get all characters
-	app.get("/all", function(req, res) {
+	app.get("/", function(req, res) {
 	  res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 
